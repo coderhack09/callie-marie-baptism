e81ccf2cfa1e57931e427d519e9d02feb3499172
Rolando Valle <rolandovalle0428@gmail.com>
2026-06-22 23:42:00 +0800

    update link "

--- lib/og-image.ts
+++ lib/og-image.ts
@@ -1,4 +1,4 @@
 import { getSiteCanonicalBase } from "@/lib/site-url"
 
-export const OG_IMAGE_PATH = "/Details/LinkPreview.png"
+export const OG_IMAGE_PATH = "/Details/newlink.png"
 export const OG_IMAGE_URL = `${getSiteCanonicalBase()}${OG_IMAGE_PATH}`
